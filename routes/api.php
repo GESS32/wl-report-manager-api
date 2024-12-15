0d1cec37e7464cf6e8f653475f1eafd11c6a7700
Uncle <uncle@webspark.com>
2024-12-15 03:15:26 +0200

    Add documentation blocks to route files

--- routes/api.php
+++ routes/api.php
@@ -2,4 +2,5 @@
 
 use Illuminate\Support\Facades\Route;
 
+/** @see routes/api/v1.php */
 Route::prefix('v1')->group(base_path('routes/api/v1.php'));
--- routes/api/v1.php
+++ routes/api/v1.php
@@ -7,6 +7,9 @@ Route::prefix('private')->group(static function (): void {
 });
 
 Route::prefix('public')->group(static function (): void {
+    /** @see routes/api/resources/auth.php */
     Route::prefix('auth')->group(base_path('routes/api/resources/auth.php'));
+
+    /** @see routes/api/resources/profile.php */
     Route::prefix('profile')->group(base_path('routes/api/resources/profile.php'));
 });
