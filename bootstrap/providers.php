34bfb798b1b7d380c2bb490920c27447608e2ecc
Uncle <uncle@webspark.com>
2025-01-04 17:22:51 +0200

    Register ApplicationUserProvider component

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,6 +2,7 @@
 
 return [
     App\Providers\InfrastructureServiceProvider::class,
+    App\Providers\ApplicationUserProvider::class,
     App\Providers\DomainUserProvider::class,
     App\Providers\DomainAuthProvider::class,
     App\Providers\DomainPromptProvider::class,
